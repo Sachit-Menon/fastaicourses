67396e933d223aaa854ad676d18f1f9ac5b0e745
Sachit-Menon <sachit.menon@duke.edu>
2017-10-21 20:50:49 +0000

    Test - adding notebook for lesson 2

--- deeplearning1/nbs/utils.py
+++ deeplearning1/nbs/utils.py
@@ -1,6 +1,6 @@
 from __future__ import division,print_function
 import math, os, json, sys, re
-import cPickle as pickle
+import pickle
 from glob import glob
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,6 +8,7 @@ from operator import itemgetter, attrgetter, methodcaller
 from collections import OrderedDict
 import itertools
 from itertools import chain
+import importlib
 
 import pandas as pd
 import PIL
